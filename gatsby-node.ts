c4cbf2e17fe09fa5ec1d2edb849a03a61883532e
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-11-28 12:29:52 -0600

    frustration

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,36 +1,68 @@
-import path from 'path';
+// import path from 'path';
 
-import type { GatsbyNode } from 'gatsby';
+// import type { GatsbyNode } from 'gatsby';
 
-type ContentfulPage = {
-    contentful_id: string;
-    slug: string;
-};
+// type ContentfulPage = {
+//     contentful_id: string;
+//     slug: string;
+// };
 
-type PageQueryResult = {
-    errors?: any;
-    data?: { allContentfulPage: { nodes: ContentfulPage[] } };
-};
+// type PageQueryResult = {
+//     errors?: any;
+//     data?: { allContentfulPage: { nodes: ContentfulPage[] } };
+// };
 
-export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const ComposablePage = path.resolve(`src/templates/composable-page.tsx`);
-    const result: PageQueryResult = await graphql(`
-        query PageGeneratorQuery {
-            allContentfulPage {
-                nodes {
-                    contentful_id
-                    slug
-                }
-            }
+// export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+//     const { createPage } = actions;
+//     const ComposablePage = path.resolve(`src/templates/composable-page.tsx`);
+//     const result: PageQueryResult = await graphql(`
+//         query PageGeneratorQuery {
+//             allContentfulPage {
+//                 nodes {
+//                     contentful_id
+//                     slug
+//                 }
+//             }
+//         }
+//     `);
+
+//     result.data?.allContentfulPage.nodes.forEach((edge: ContentfulPage) => {
+//         createPage({
+//             path: `${edge.slug}`,
+//             component: ComposablePage,
+//             context: { slug: edge.slug }
+//         });
+//     });
+// };
+
+import { env } from 'node:process';
+import type { GatsbyConfig } from 'gatsby';
+import 'dotenv/config';
+let accessToken = env['CONTENTFUL_DELIVERY_TOKEN'];
+let host: string = 'cdn.contentful.com';
+if (env['NODE_ENV'] === 'development') {
+    accessToken = env['CONTENTFUL_PREVIEW_TOKEN'];
+    host = 'preview.contentful.com';
+}
+const config: GatsbyConfig = {
+    graphqlTypegen: true,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-image'
+        },
+        {
+            options: {
+                accessToken,
+                environment: env['CONTENTFUL_ENVIRONMENT'],
+                host,
+                spaceId: env['CONTENTFUL_SPACE_ID']
+            },
+            resolve: 'gatsby-source-contentful'
         }
-    `);
-
-    result.data?.allContentfulPage.nodes.forEach((edge: ContentfulPage) => {
-        createPage({
-            path: `${edge.slug}`,
-            component: ComposablePage,
-            context: { slug: edge.slug }
-        });
-    });
+    ],
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'My Gatsby Site'
+    }
 };
+export default config;
--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,6 +1,16 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { ContentfulContentSource } from '@stackbit/cms-contentful'
+import { defineStackbitConfig } from '@stackbit/types'
 
 export default defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
-    ssgName: 'gatsby'
-});
+  stackbitVersion: '~0.6.0',
+  ssgName: 'gatsby',
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+    }),
+  ],
+})
+
